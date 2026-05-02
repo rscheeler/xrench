521d8147efbacd064f363cee15b36cc1b30933e4
Rob Scheeler <robscheeler@gmail.com>
2026-05-02 16:49:32 -0600

    Get version and bump version capabilities

--- src/xrench/__init__.py
+++ src/xrench/__init__.py
@@ -1,1 +1,9 @@
-"""xrench."""
+"""xrench: to support RF and microwave design Python tools."""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("xrench")
+except PackageNotFoundError:
+    # Package is not installed
+    __version__ = "0.0.0-dev"
